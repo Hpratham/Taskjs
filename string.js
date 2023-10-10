eb616d864b9489b7c93b41636688cfa7fec3aaba
Hpratham <143834870+Hpratham@users.noreply.github.com>
2023-10-10 10:33:50 +0000

    String

---
+++ string.js
@@ -0,0 +1,36 @@
+//string methods
+//  const name="Prathmesh"
+//  const repoCount=50
+//  console.log('Hello my name is ${name}and my repo count is ${repoCount}');
+
+const gameName= new String('prathmesh-hp')
+console.log(gameName[0]);
+console.log(gameName.___proto___);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(typeof gameName);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('t'));
+
+const newString =gameName.slice(2,-4);
+console.log(newString);           
+// the amzing of the slice is the you can add negative value in this code or function.
+
+const newStringone="    Bhiaya    "
+console.log(newStringone);
+
+
+const url="https://fluffy-acorn-g4qp75x4wr5qhggp.github.dev/"
+console.log(url.replace('fluffy-acorn','prathmeshhiregroup'))
+
+console.log(url.includes('sundar')); //in the given code you have to find thos character are also available in the code or not
+
+console.log(gameName.split('-'));
+
+
+for any kind of information follow the site MDN JS
+
